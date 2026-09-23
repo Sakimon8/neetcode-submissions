191fbf20bdf7b16b67f9a493cacee93c37d36205
Sakimon8 <55884639+Sakimon8@users.noreply.github.com>
2026-09-24 01:34:14 +0530

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        boolean columns[][] = new boolean[9][9];
+        boolean rows[][] = new boolean[9][9];
+        boolean subMatrix[][] = new boolean[9][9];
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
+                    int val = board[i][j] - '1';
+                    int subMatrixValue = (i / 3) * 3 + j / 3;
+                    if (rows[i][val] == false && columns[j][val] == false
+                        && subMatrix[subMatrixValue][val] == false) {
+                        rows[i][val] = true;
+                        columns[j][val] = true;
+                        subMatrix[subMatrixValue][val] = true;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+}
